e6d919032d6c0f7902600459af22bbc3518c11f9
179Bell <shingo.msdf@gmail.com>
2022-12-07 22:19:04 +0900

    add /theme

---
+++ src/themes/index.ts
@@ -0,0 +1,13 @@
+import colors from './colors'
+import fontSizes from './fontSize'
+import letterSpacings from './letterSpacing'
+import lineHeights from './lineHeights'
+import space from './space'
+
+export const theme = {
+  colors,
+  lineHeights,
+  letterSpacings,
+  fontSizes,
+  space,
+} as const
