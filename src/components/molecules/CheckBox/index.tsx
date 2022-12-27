f76bd1a6b87aeb02f6db88a3c19de71404bfa752
179Bell <shingo.msdf@gmail.com>
2022-12-28 08:19:00 +0900

    add molecules/CheckBox

---
+++ src/components/molecules/CheckBox/index.tsx
@@ -0,0 +1,69 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import styled from "styled-components";
+import {
+  CheckBoxOutlineBlankIcon,
+  CheckBoxIcon,
+} from "components/atoms/IconButton";
+import Flex from "components/layout/Flex";
+
+export interface CheckBoxProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "dafaultValue"> {
+  /**表示ラベル */
+  label?: string;
+}
+
+// 非表示のチェックボックス
+const CheckBoxElement = styled.input`
+  display: none;
+`;
+
+// チェックボックスのラベル
+const Label = styled.label`
+  cursor: pointer;
+  margin-left: 6px;
+  user-select: none;
+`;
+
+/**チェックボックス */
+const CheckBox = (props: CheckBoxProps) => {
+  const { id, label, onChange, checked, ...rest } = props;
+  const [isChecked, setIsChecked] = useState(checked);
+  const ref = useRef<HTMLInputElement>(null);
+  const onClick = useCallback(
+    (e: React.MouseEvent) => {
+      e.preventDefault();
+      // チェックボックスを強制的にクリック
+      ref.current?.click();
+      setIsChecked((isChecked) => !isChecked);
+    },
+    [ref, setIsChecked]
+  );
+
+  useEffect(() => {
+    // パラメータからの変更を受け付ける
+    setIsChecked(checked ?? false);
+  }, [checked]);
+
+  return (
+    <>
+      <CheckBoxElement
+        {...rest}
+        ref={ref}
+        type="checkbox"
+        checked={checked}
+        readOnly={!onChange}
+        onChange={onChange}
+      />
+      <Flex alignItems="center">
+        {/* チェックボックスのon/offの描画 */}
+        {checked ?? isChecked ? (
+          <CheckBoxIcon size={20} onClick={onClick} />
+        ) : (
+          <CheckBoxOutlineBlankIcon size={20} onClick={onClick} />
+        )}
+      </Flex>
+    </>
+  );
+};
+
+export default CheckBox;
